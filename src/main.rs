d0a16e411d0a6060f8155955d464129453cfbe5c
ranafge <ranafge@gmail.com>
2024-01-25 11:13:15 +0600

    select tasks async operation TcpSteam add as async complex operation

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,62 @@
+use alloc::task;
 use tokio::sync::oneshot;
 
-#[tokio::main]
-async fn main() {
-    let (tx1, rx1) = oneshot::channel();
-    let (tx2, rx2) = oneshot::channel();
+// #[tokio::main]
+// async fn main() {
+//     let (tx1, rx1) = oneshot::channel();
+//     let (tx2, rx2) = oneshot::channel();
 
-    tokio::spawn(async {
-        let _ = tx1.send("one");
-        // _ is a place holder for a value of the `tx1.send("one");` but here intentionally ignore
-    });
-    tokio::spawn(async {
-        let _ = tx2.send("two");
-    });
-    // there are two channel to send message first one is send `one` and second channel send  'two'
+//     tokio::spawn(async {
+//         let _ = tx1.send("one");
+//         // _ is a place holder for a value of the `tx1.send("one");` but here intentionally ignore
+//     });
+//     tokio::spawn(async {
+//         let _ = tx2.send("two");
+//     });
+//     // there are two channel to send message first one is send `one` and second channel send  'two'
 
-    // in tokio::select wait for a message to complete one . there are two branch of val 
-    // if rx1 or rx2 is complete message will print for that complete first. other one will be drop 
-    // drop or cancellatin occure by dropin future
-    tokio::select! {
-        // val is a pttern = rx1 is async operion `{}` block is handler
-        val = rx1 => {
-            println!("rx1 completed first with {:?}", val);
-        }
-        val = rx2 => {
-            println!("rx2 completed first with {:?}", val)
-        }
-    }
-}
+//     // in tokio::select wait for a message to complete one . there are two branch of val 
+//     // if rx1 or rx2 is complete message will print for that complete first. other one will be drop 
+//     // drop or cancellatin occure by dropin future
+//     tokio::select! {
+//         // val is a pttern = rx1 is async operion `{}` block is handler
+//         val = rx1 => {
+//             println!("rx1 completed first with {:?}", val);
+//         }
+//         val = rx2 => {
+//             println!("rx2 completed first with {:?}", val)
+//         }
+//     }
+// }
 
 // if <pattern> = <async operation>  
 //when async operation result match with pattern then cancell other branches and execute handler=> <handler>
+
+// select takes async operation
+
+
+
+
+// Select takes async operation
+
+use tokio::net::TcpStream;
+
+#[tokio::main]
+
+async fn main() {
+    let (tx, rx) = oneshot::channel();
+
+
+    tokio::spawn(async move {
+        tx.send("done").unwrap()
+    });
+
+    tokio::select! {
+        socket  = TcpStream::connect("localhost::3465") => {
+            println!("Socket connectd {:?}", socket) ;
+        }
+        msg  = rx  => {
+            println!("receive mesage first {:?}", msg)
+        }
+    }
+}
